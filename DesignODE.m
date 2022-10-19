1479ee8338f34742727355730c02dc86c3e14c88
Abigail <abigailviolet6@gmail.com>
2022-10-19 02:01:06 -0600

    Round 1 cause we die like men
    cries

---
+++ DesignODE.m
@@ -0,0 +1,159 @@
+function [Homework8_ODE] = DesignODE(V,Y)
+%% output variables 
+
+%Chemical Species molar flow rates
+F_Cl2Eth = Y(1); 
+F_C2H4 = Y(2); %
+F_HCl = Y(3); %
+F_O2 = Y(4); %
+F_CO2= Y(5);
+F_H2O =Y(6);
+F_Cl3Eth = Y(7);
+F_Cl2 =Y(8);
+
+%Changing F total
+F = F_Cl2Eth+F_C2H4+F_HCl+F_O2+F_CO2+F_H2O+F_Cl3Eth+F_Cl2; 
+
+%temperature of the tube and the coolant
+T = Y(9);
+Tc = Y(10);
+
+%kPa go back and check this
+P_in = Y(11); %pressure in 
+
+%Homework8_ODE = [d_product; d_C2H4; d_HCl; d_O2; d_CO2; d_H2O; d_Cl3Eth; d_Cl2; d_T_Tube; d_T_Coolant;d_P];
+%% Defining Constants 
+
+%THE GUESSES
+% The following values are values that we choose/guess
+% reactor length is also unknown, but we calculate
+numTubes = 1; %this must be an integer
+tubeDiameter = 1; %We will decide this here, because we have some 
+% engineering intuition here
+T_i = 1; %Temperature in
+
+%this is initial total volumetric flow rates combining all the tubes [m^3/hr]
+Product_in_vol = 0;
+C2H4_i_vol= 1729.38;
+HCl_i_vol = 160943.2;
+O2_i_vol = 3724.53;
+CO2_i_vol = 0;
+H2O_i_vol = 0;
+Cl3Eth_i_vol = 0;
+Cl2_i_vol = 33.789;
+%Inital volumetric flow rate in
+VolFlow_i = C2H4_i_vol + HCl_i_vol + O2_i_vol + CO2_i_vol + H2O_i_vol + Cl3Eth_i_vol + Cl2_i_vol; 
+
+%this is initial molar flow rates combining all the tubes [mol/hr]
+F_Product_in = 0;
+F_C2H4_i= 1;
+F_HCl_i = 1;
+F_O2_i= 1;
+F_CO2_i = 0;
+F_H2O_i = 0;
+F_Cl3Eth_i = 0;
+F_Cl2_i = 1;
+%initial molar flow rate  is constant
+molFlow_i = F_C2H4_i + F_HCl_i + F_O2_i + F_CO2_i + F_H2O_i + F_Cl3Eth_i + F_Cl2_i; 
+% divide by the number of tubes so it's a single tube
+F_0_single = molFlow_i/numTubes;
+
+Tau = 3.6; %space time in seconds 
+reactorVol = Tau/3600*VolFlow_i; %3600 to convert from s to hr
+%cross sectional area
+Ac=pi*(tubeDiameter/2)^2;
+%here, Matlab calculates a reactor length
+reactorLength = reactorVol/numTubes/Ac;
+%output reactor length
+
+
+
+%heat capacities 
+CP_C2H4=1;
+CP_Cl2Eth=1;
+CP_HCl=1;
+CP_O2=1;
+CP_CO2=1;
+CP_H2O=1;
+CP_Cl3Eth=1;
+CP_C12=1;
+CP_C=1; %Coolant heat capacity
+
+
+%other constants guess and check 
+superficVelocity = 1; %superficial velocity
+G = 1 ;%gravity 
+g_c = 1;%conversion (in metric units)
+D_p = 1;%bead diameter
+Void = 1; %void fraction
+mu = 1; %gas viscosity
+M_DotC = 1; %mass flow of the coolant
+U = 1; %Overall heat transfer coefficient
+A = Diameter*pi*L;
+rho = 1; %initial gas density for the ergun equation
+T_0 =1; %inlet temperature
+%% Heats of Reaction
+H_1 =1;
+H_2=1;
+H_3=1;
+H_4=1;
+
+%% Base rate equations 
+K_1 = 10^(4.2)*exp(-40.1/(8.314*T)); %K_1 temperature Arrhenius equation
+R_1 = K_1*(F_C2H4*(F_Cl2^0.5)/(F^(1.5)))*P^(1.5);
+
+K_2 = 10^(13.23)*exp(-128.04/(8.314*T));
+R_2 = K_2*(F_Cl2Eth*(F_Cl2^0.5)/(F^1.5))*P^1.5;
+
+K_3 =10^(6.78)*exp(-112/(8.314*T));
+R_3 = K_3*(F_O2*(F_Cl2^0.5)*(F_C2H4)/(F^2.5))*P^2.5;
+
+K_4f = 1000*exp(5.4+(160000/(1.987*T)));
+K_4b = exp(5.4+(160000/(1.987*T)));
+%*****we are using the experimental values found in the paper not the 2
+%termed rate equation
+K_4 = K_4f/K_4b;
+R_4 = K_4*((F_O2)/(F_Cl2)); %check the units for k4 and make sure that works out
+
+%% Species balances
+C2H4 = R_1 + R_3;
+Cl2Eth = R_2-R_1;
+HCl = 2*R_1 + R_2 + R_4;
+O2 = 0.5*R_1+ 0.5*R_2+ 3*R_3 + 0.5*R_4;
+CO2 = -2*R_3;
+H2O = -R_1 -R_2 -2*R_3 -R_4;
+Cl3Eth = -R_2;
+Cl2= -R_4;
+F = C2H4+ Cl2Eth+HCl+O2+CO2+H2O+Cl3Eth+Cl2; %total flow rate based off of species balance (changes)
+
+%% Thermal Balances 
+%per tube 
+Top = (R_1 * H_1 + R_2*H_2 + R_3*H_3 + R_4*H_4)*T - U*A*(T-Tc);
+Bot = (F_C2H4*CP_C2H4) + (F_Cl2Eth*CP_Cl2Eth) + (F_HCl*CP_HCl) + (F_O2*CP_O2) + (F_CO2*CP_CO2) + (F_H2O*CP_H2O) + (F_Cl3Eth*CP_Cl3Eth)+(F_Cl2 + CP_C12);
+Tube_balance = n*Top/Bot;
+
+%Coolant fluid
+Coolant_Balance = U*A*(T-Tc)/(M_DotC*CP_C);
+
+%% Pressure Considerations %which we also have to guess and check.....
+Partone = G*(1-Void)/(rho*g_c*D_p*(Void^3)); 
+Parttwo = ((150*(1-Void)*mu)/D_p)+(1.75*G);
+Beta = Partone*Parttwo;
+
+Pressure_Change = Beta*P_in*T*F/(Ac*P*T_0*F_0);
+
+%% the end?
+d_product = Cl2Eth;
+d_C2H4 = C2H4;
+d_HCl = HCl;
+d_O2 = O2;
+d_CO2 = CO2;
+d_H2O = H2O;
+d_Cl3Eth = Cl3Eth;
+d_Cl2 = Cl2;
+d_T_Tube = Tube_balance;
+d_T_Coolant = Coolant_Balance;
+d_P = Pressure_Change;
+
+Homework8_ODE = [d_product; d_C2H4; d_HCl; d_O2; d_CO2; d_H2O; d_Cl3Eth; d_Cl2; d_T_Tube; d_T_Coolant;d_P];
+end 
---
+++ HomeworkEightDesign.m
@@ -0,0 +1,94 @@
+% This code is for CHEN 4520 Chemical Process Synthesis (Fall 2022)
+% Homework 8
+% Written by: Aimee Maravi, Abigail Hutabarat, Mady Murphy, 
+% Sean Oishi-Holder, and Mohammed Alahmed
+
+T_out = 533; %outlet Temperature in kelvin 
+Cbd = 1975; %kg cat/m^3 this is catalyst bulk density 
+Void = 0.50; %catalyst voidage
+%heurisitc to account for later: catalyst particle diameter is an eigth of
+%the tube diameter
+
+%REE
+%% Initial Conditions
+%T0 = 1; %what units 
+
+%this is molar flow rates [mol/hr]
+F_Product_in = 0;
+F_C2H4_i= 1;
+F_HCl_i = 1;
+F_O2_i= 1;
+F_CO2_i = 0;
+F_H2O_i = 0;
+F_Cl3Eth_i = 0;
+F_Cl2_i = 1;
+%initial molar flow rate  is constant
+molFlow_i = F_C2H4_i + F_HCl_i + F_O2_i + F_CO2_i + F_H2O_i + F_Cl3Eth_i + F_Cl2_i; 
+
+%Guess T initial
+Tube_temp_in=0;
+Coolant_temp_in = 0;
+Pressure_i = 2000; %inlet pressure in kPa
+IC = [F_Product_in, F_C2H4_i, F_HCl_i, F_O2_i, F_CO2_i, F_H2O_i, F_Cl3Eth_i, F_Cl2_i, Tube_temp_in, Coolant_temp_in, Pressure_i];
+%Homework8_ODE = [d_product; d_C2H4; d_HCl; d_O2; d_CO2; d_H2O; d_Cl3Eth; d_Cl2; d_T_Tube; d_T_Coolant;d_P];
+%% this is my Domain
+V_I = 0;
+%guessing the length is of the reactor is z? do we also just guess and
+%check this value???????????????/
+V_Domain = [V_I 1000]; %Define the temperature domain
+%% SOLVE ODE 
+[Vsol, Ysol] = ode45('DesignODE', V_Domain, IC);
+
+%% Data handling 
+% Extraction Solutions for each variable 
+Product_sol = Ysol (:, 1) ;
+C2H4_sol = Ysol(:,2); 
+HCl_sol = Ysol(:,3);
+O2_sol = Ysol(:,4);
+CO2_sol = Ysol(:,5);
+H2O_sol = Ysol(:,6);
+Cl3Eth_sol = Ysol(:,7); 
+F_Cl2_i = Ysol(:,8);
+TubeTemp_sol = Ysol(:,9);
+CoolantTemp_sol = Ysol(:,10);
+Pressure_sol = Ysol(:,11);
+
+%% plotting 
+
+fig1 = figure(1);
+set(fig1, 'name', 'FlowRates');
+%I realized we need a reactor volume rate
+area(Product_sol,Vsol);
+xlabel('Reactor Volume (L)');
+ylabel('FlowRates');
+hold on 
+plot(C2H4_sol)
+hold on 
+plot(HCl_sol)
+hold on 
+plot(O2_sol)
+hold on 
+plot(H2O_sol)
+hold on
+plot(Cl3Eth_sol)
+hold on
+plot(F_Cl2_i)
+
+fig2 = figure(2);
+set(fig2,'name', 'Reactor Temperature Profile')
+area(Vsol, TubeTemp_sol);
+xlabel('Tube Volume');
+ylabel('Temperature ');
+
+fig3 = figure(3); 
+set(fig2,'name', 'mAb')
+area(Vsol, CoolantTemp_sol);
+xlabel('Reactor Volume');
+ylabel('Coolant Temperature');
+%we need reactor pressure- thats probably what one of the ODEs we're
+%missing currentlly 
+
+%
+%points of confusion for me 1) what the heck is the units of Ysol?
+%Xsol will be in units of volume
+%are we integrating by 2 variables? 
